c4ac3154269f71294d052fba11e0afef8546fe96
BakJakub <kuba.bak.1997@gmail.com>
2023-12-04 11:58:42 +0100

    Initial Commit

---
+++ Event Cinemas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Event Cinemas
+//
+//  Created by Jakub Bak on 04/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
